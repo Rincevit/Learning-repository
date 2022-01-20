4e49693fac72bd40ca135e827fdee3ea7dcba26c
Jakub Mathias <96866266+Rincevit@users.noreply.github.com>
2022-01-20 10:48:10 +0100

    Add files via upload

---
+++ Spring-learninig/src/main/java/com/example/springlearninig/SpringLearninigApplication.java
@@ -0,0 +1,13 @@
+package com.example.springlearninig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearninigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLearninigApplication.class, args);
+    }
+
+}
