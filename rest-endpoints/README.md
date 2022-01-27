5637af740d6a48448057fd0cbdeb548df7fb663b
Rincevid <rincewit@gmail.com>
2022-01-27 09:50:20 +0100

    The /payment endpoint's implementation in the controller class

---
+++ rest-endpoints/README.md
@@ -0,0 +1,1 @@
+#Implementing REST endpoints with Spring
---
+++ rest-endpoints/src/test/java/com/example/restendpoints/RestEndpointsApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.restendpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestEndpointsApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
